58f5717be2a1f2690e4530da137f5ca0c04792a1
CanhNT <nguyentrancanh81@gmail.com>
2022-10-06 08:55:32 +0700

    add notification plugin to export pdf pluginn

--- src/plugins/export-pdf/export-pdf.js
+++ src/plugins/export-pdf/export-pdf.js
@@ -1,11 +1,12 @@
 import { Plugin } from '@ckeditor/ckeditor5-core';
+import { Notification } from '@ckeditor/ckeditor5-ui';
 import ExportPdfUI from './export-pdf-ui';
 
 import './theme/export-pdf.css';
 
 export class ExportPdf extends Plugin {
   static get requires() {
-    return [ExportPdfUI];
+    return [ExportPdfUI, Notification];
   }
 
   static get pluginName() {
